ec0fab325f7391bff8520ce7cc7dd46b9282e82f
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-05-11 15:33:16 -0400

    Small change to thawing of 2017-12-19 sample

--- thaw_2017_12_19_sample.m
+++ thaw_2017_12_19_sample.m
@@ -1,3 +1,3 @@
 do_verify = false ;
 do_on_cluster = true ;
-mj2tif('/groups/mousebrainmicro/mousebrainmicro/data/2017-12-19/Tiling', '/groups/mousebrainmicro/mousebrainmicro/data/2017-12-19-thawed', do_verify, do_on_cluster) ;
+mj2tif('/groups/mousebrainmicro/mousebrainmicro/data/2017-12-19', '/groups/mousebrainmicro/mousebrainmicro/data/2017-12-19-thawed', do_verify, do_on_cluster) ;
