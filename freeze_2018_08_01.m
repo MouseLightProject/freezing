80284934c3b2f947435c3af6b9e5d160b93d554e
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-11-08 14:35:06 -0500

    Changed some of the per-sample scripts to use freeze_sample.m

--- freeze_2018_08_01.m
+++ freeze_2018_08_01.m
@@ -5,5 +5,4 @@ compression_ratio = 10 ;
 do_verify = false ;
 do_run_on_cluster = true ;
 
-mj2_from_tif(mj2_output_folder_path, tif_input_folder_path, compression_ratio, do_verify, do_run_on_cluster)
-is_all_well = verify_frozen_files(mj2_output_folder_path, tif_input_folder_path, do_run_on_cluster) 
+freeze_sample
