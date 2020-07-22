801f55776d08b721e7c04f7845c7f3bc4110c958
Adam L. Taylor <taylora@janelia.hhmi.org>
2020-07-22 13:40:11 -0400

    Add a few more per-sample scripts

---
+++ freeze_2020_02_24.m
@@ -0,0 +1,7 @@
+sample_date = '2020-02-24' ;
+mj2_output_folder_path = sprintf('/nrs/mouselight-v/frozen/%s', sample_date) ;
+tif_input_folder_path = sprintf('/groups/mousebrainmicro/mousebrainmicro/data/%s', sample_date) ;
+compression_ratio = 10 ;
+do_run_on_cluster = true ;
+
+freeze_sample
