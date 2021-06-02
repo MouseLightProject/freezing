700a0733f4262ccf501cff3ba35a68f0739ea2f2
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-06-01 21:15:23 -0400

    Added tmt submodule, updated modpath.m

--- modpath.m
+++ modpath.m
@@ -1,3 +1,5 @@
 function modpath()
-    addpath(genpath('mouselight_toolbox')) ;
+    this_folder_path = fileparts(mfilename('fullpath')) ;
+    tmt_modpath_path = fullfile(this_folder_path, 'tmt', 'modpath.m') ;
+    run(tmt_modpath_path) ;
 end
